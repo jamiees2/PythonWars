fdcb4bfbc93a60d558ad84189260add7db8a3beb
Elísa <elisa14@ru.is>
2016-12-18 15:35:45 +0000

    Score Model

--- pythonwars/models.py
+++ pythonwars/models.py
@@ -22,3 +22,22 @@ class User(db.Model):
         return '<User ({username})>'.format(
             username=self.username
         )
+
+class Score(db.Model):
+    __tablename__ = 'score'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship("User", uselist=False)
+    level = db.Column(db.String)
+    steps = db.Column(db.Integer)
+    length = db.Column(db.Integer)
+    code = db.Column(db.Text)
+    score = db.Column(db.Integer)
+
+    def __init__(self, user, level, steps, length, code, score):
+        self.user_id = user.id
+        self.level = level
+        self.steps = steps
+        self.length = length
+        self.code = code
+        self.score = score
